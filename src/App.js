0e63a9177dd8ca91b40532911e4d8495c221a1dc
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-09-22 13:28:46 +0200

    add basic mapbox implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import './App.css';
+import { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidmFsZW50aW5sdyIsImEiOiJja3JxOW5yeWUxeGVtMzBsNzc0aWFra3JrIn0.wwLgusrB4U7Cpb0q70g2tw';
 
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+    map.current.on('load', function() {
+      map.current.addLayer({
+        id: 'country-boundaries',
+        source: {
+          type: 'vector',
+          url: 'mapbox://mapbox.country-boundaries-v1',
+        },
+        'source-layer': 'country_boundaries',
+        type: 'fill',
+        paint: {
+          'fill-color': '#d2361e',
+          'fill-opacity': 0.4,
+        },
+      },'country-label');
+    });
+  });
+
   return (
     <div className="App">
+      <div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
     </div>
   );
 }
