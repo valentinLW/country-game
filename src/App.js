8ca8251f93bb7b13edfa995ed07ee52fe7c8dfe8
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-09-22 14:02:38 +0200

    add iso codes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import isocodes from './isocodes.json'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidmFsZW50aW5sdyIsImEiOiJja3JxOW5yeWUxeGVtMzBsNzc0aWFra3JrIn0.wwLgusrB4U7Cpb0q70g2tw';
 
@@ -8,12 +9,17 @@ function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
 
+  const [guessed, setGuessed] = useState([]);
+
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11'
+      style: 'mapbox://styles/mapbox/streets-v8'
     });
+
+    console.log(isocodes);
+
     map.current.on('load', function() {
       map.current.addLayer({
         id: 'country-boundaries',
@@ -27,8 +33,16 @@ function App() {
           'fill-color': '#d2361e',
           'fill-opacity': 0.4,
         },
-      },'country-label');
+      });
+
+      map.current.setFilter('country-boundaries', [
+        "in",
+        "iso_3166_1_alpha_3",
+        'NLD',
+        'ITA'
+      ]);
     });
+
   });
 
   return (
